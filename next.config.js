9adbd4fda0797b8851a463f2cff59fae96e7d71c
yasindu-tech <166025746+Yasindu-Gmg@users.noreply.github.com>
2025-05-03 23:25:40 +0530

    refactor: update serverActions configuration in Next.js to use object format with allowedOrigins

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'startuphub.vercel.app'],
+    },
   },
   webpack: (config) => {
     config.resolve.alias = {
