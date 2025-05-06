55d7069c6672bde18e4c2803c66be2ec8a71265c
yasindu-tech <166025746+Yasindu-Gmg@users.noreply.github.com>
2025-05-06 13:12:55 +0530

    refactor: update allowedOrigins in serverActions for improved CORS support

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000', 'startuphublk.netlify.app'],
+      allowedOrigins: ['localhost:3000', 'startuphublk.netlify.app', 'https://startuphublk.netlify.app'],
     },
   },
   webpack: (config) => {
